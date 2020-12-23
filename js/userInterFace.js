f5d1994b36c9242f0ea22f4b3e27e203dbdad3a9
msalomonsson <69800375+msalomonsson@users.noreply.github.com>
2020-12-23 09:55:57 +0100

    fixed render todo.json

--- js/userInterFace.js
+++ js/userInterFace.js
@@ -7,6 +7,21 @@ class Ui {
         const tasklist = document.querySelector('.task-list')
 
 
+        for(let i = 0; i < taskObj.length; i++){
+
+            tasklist.innerHTML += `
+            <li class="task-item">
+                <div class="left-content">
+                    <i class="far fa-check-circle"></i>
+                    <p>${taskObj[i].task}</p>
+                </div>
+                <i class="fas fa-trash"></i>
+            </li>
+        `;
+        }
+
+
+
     }
 
 
